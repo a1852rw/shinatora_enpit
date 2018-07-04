0247aac817104d65e0c7b2f3e503144840eb6d77
JINJW <a1824jj@aiit.ac.jp>
2018-07-04 10:01:33 +0000

    hello.rb fix

--- hello.rb
+++ hello.rb
@@ -3,3 +3,16 @@ require 'sinatra'
 get '/' do
   "Hello World!"
 end
+
+get '/path/to' do
+  "this is [/path/to]"
+end
+
+get '/hello/*' do |name|
+  "Hello #{name}. how are you?"
+end
+
+get "/google" do
+  require "httparty"
+  HTTParty.get('http://google.com', follow_redirects: true).body
+end
