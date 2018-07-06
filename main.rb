59cb30835897fa4ea675f8596a8ca4d9f4039213
a16z8yy@aiit.ac.jp <a16z8yy@aiit.ac.jp>
2018-07-06 10:24:01 +0000

    complete form page

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 
-set :public_folder, File.dirname(__FILE__) + '/static'
 
-get '/main' do
-    erb :indexi
+get '/' do
+    @hello = "こんにちは、hogehoge!"
+    erb :index
 end
 
-post '/' do
-    
+post '/form' do
+    @input1 = params[:input1]
+    @hello = @input1
+    erb :index
 end
