582bddb648c6c04eee4d7882f0f19ef009ba636c
a16z8yy@aiit.ac.jp <a16z8yy@aiit.ac.jp>
2018-07-20 10:35:48 +0000

    Add sunitize code to fix XSS vulnerability

--- main.rb
+++ main.rb
@@ -11,6 +11,12 @@ end
 
 post '/login' do
     @uname = params[:uname]
+    if @uname == ""
+        @uname = "Guest"
+    else
+        @uname = @uname.gsub(/</, "&lt")
+        @uname = @uname.gsub(/>/, "&gt")
+    end
     erb :message
 end
 
@@ -25,6 +31,8 @@ end
 post '/message' do
     @uname = params[:uname]
     @message = "#{(Time.now).strftime("%Y/%m/%d %H:%M")}" + "  " + @uname + "-san wrote:<br>" + params[:message]
+    @message = @message.gsub(/</, "&lt")
+    @message = @message.gsub(/>/, "&gt")
     @@messages = @message + "<br><br>" + @@messages
     puts @@messages
     erb :message
