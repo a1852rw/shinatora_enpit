6bb967a41537e008f69afc4df989a204d1b7d97d
jin <a1824jj@aiit.ac.jp>
2018-07-26 01:26:25 +0000

    main.rb 修正

--- main.rb
+++ main.rb
@@ -1,39 +1,70 @@
 require 'sinatra'
+require 'json'
+require 'date'
+require File.expand_path(File.dirname(__FILE__) + '/jsontest/JsonHelper.rb')
+require File.expand_path(File.dirname(__FILE__) + '/jsontest/JsonData.rb')
+
+j = J_Data.getData
+puts j
+
+before do
+  puts "!Before start!"
+  puts  Time.now
+
+  puts params
+#  puts params.count if !params.count.blank?
+
+  if (params.count == 2 &&  params[:name] != "" &&  params[:context] != "null")
+    element = {
+                JSON_KeyWord.id => j["data"].count + 1,
+                JSON_KeyWord.name => params[:name],
+                JSON_KeyWord.context => params[:context],
+                JSON_KeyWord.date => Date.today()
+               }
+    J_Data.putData(element)
+    j = J_Data.getData
+  end
+
+  puts "!Before end!"
+end
+
+after do
+  puts "!After start!"
+  puts Time.now
+  puts "!After end!"
+end
+
 
 get "/" do
     erb :index
 end
 
 get '/login' do
-    @uname = ""
+    @name = ""
     erb :login
 end
 
 post '/login' do
-    @uname = params[:uname]
-    if @uname == ""
-        @uname = "Guest"
+    @name = params[:name]
+    if @name == ""
+        @name = "Guest"
     else
-        @uname = @uname.gsub(/</, "&lt")
-        @uname = @uname.gsub(/>/, "&gt")
+        @name = @name.gsub(/</, "&lt")
+        @name = @name.gsub(/>/, "&gt")
     end
+    @raw = j["data"]
     erb :message
 end
 
 
-@@messages = ""
-
 get '/message' do
-    
+    @name = params[:name]
+    @raw = j["data"]
     erb :message
 end
 
 post '/message' do
-    @uname = params[:uname]
-    @message = "#{(Time.now).strftime("%Y/%m/%d %H:%M")}" + "  " + @uname + "-san wrote:<br>" + params[:message]
-    @message = @message.gsub(/</, "&lt")
-    @message = @message.gsub(/>/, "&gt")
-    @@messages = @message + "<br><br>" + @@messages
-    puts @@messages
+    @name = params[:name]
+    @raw = j["data"]
     erb :message
 end
